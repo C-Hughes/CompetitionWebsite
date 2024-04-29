dff3d73b5ee98cca00c71dfbd21b7567244a3e92
CH <connor-hughes@outlook.com>
2024-04-29 21:55:13 +0100

    Nav updated, faq added

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Giveaway' });
 });
 
+
+router.get('/faq', function(req, res, next) {
+  res.render('faq', { title: 'FAQ' });
+});
+
 module.exports = router;
