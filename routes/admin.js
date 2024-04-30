79d1075f2f39d05ca8c70678c365737d18da5054
CH <connor-hughes@outlook.com>
2024-04-30 19:19:27 +0100

    Added admins pages and routes

--- routes/admin.js
+++ routes/admin.js
@@ -3,9 +3,20 @@ var router = express.Router();
 
 /* GET admin listings. */
 router.get('/', function(req, res, next) {
-  res.render('admin/dashboard', { title: 'Dashboard' });
+  res.render('admin/dashboard', { title: 'Dashboard', active: { dashboard: true }  });
 });
 
+router.get('/winners', function(req, res, next) {
+    res.render('admin/winners', { title: 'Winners', active: { winners: true } });
+  });
+  
+  router.get('/discounts', function(req, res, next) {
+    res.render('admin/discounts', { title: 'Discounts', active: { discounts: true } });
+  });
+  
+  router.get('/users', function(req, res, next) {
+    res.render('admin/users', { title: 'Users', active: { users: true } });
+  });
 
 
 module.exports = router;
