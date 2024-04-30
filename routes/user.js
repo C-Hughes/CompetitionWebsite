4a1279604bb091d83bbd6095fe95b7ecd2cffc09
CH <connor-hughes@outlook.com>
2024-04-30 15:31:42 +0100

    Added user pages and routes

--- routes/user.js
+++ routes/user.js
@@ -7,5 +7,21 @@ router.get('/', function(req, res, next) {
   res.render('user/dashboard', { title: 'My Account' });
 });
 
+router.get('/address', function(req, res, next) {
+  res.render('user/address', { title: 'Address' });
+});
+
+router.get('/accountDetails', function(req, res, next) {
+  res.render('user/accountDetails', { title: 'Account Details' });
+});
+
+router.get('/communicationPreferences', function(req, res, next) {
+  res.render('user/communicationPreferences', { title: 'Communication Preferences' });
+});
+
+router.get('/safePlaying', function(req, res, next) {
+  res.render('user/safePlaying', { title: 'Safe Playing' });
+});
+
 
 module.exports = router;
