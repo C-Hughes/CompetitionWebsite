2fad96fb51917c9f6523fdb56f4498e3271a45d5
CH <connor-hughes@outlook.com>
2024-05-02 19:41:28 +0100

    Ticket slider and buttons working

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -6,15 +6,28 @@ function updateTicketCount(change){
 
   var ticketCount = sliderWithValue.value;
 
+  //If buttons pressed or slider moved
   if (change == '-') {
-    console.log('- pressed');
     ticketCount--;
-    ticketSliderCount.innerText = ticketCount;
+
+    //Make sure you cannot add more than max or less than min number of tickets.
+    if (ticketCount < sliderWithValue.min) {
+      ticketCount = sliderWithValue.min;
+      ticketButtonCount.innerText = "Number of tickets: "+ ticketCount +"";
+      sliderWithValue.value = ticketCount;
+    }
   } else if (change == '+'){
-    console.log('+ pressed');
     ticketCount++;
-    ticketSliderCount.innerText = ticketCount;
+
+    //Make sure you cannot add more than max or less than min number of tickets.
+    if (ticketCount > sliderWithValue.max) {
+      ticketCount = sliderWithValue.max;
+      ticketButtonCount.innerText = "Number of tickets: "+ ticketCount +"";
+      sliderWithValue.value = ticketCount;
+    }
   }
+ 
   ticketButtonCount.innerText = "Number of tickets: "+ ticketCount +"";
   sliderWithValue.value = ticketCount;
+  ticketSliderCount.innerText = ticketCount;
 }
