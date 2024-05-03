ebeda466d5cc18e754578ac27787abc4236611d7
CH <connor-hughes@outlook.com>
2024-05-03 11:12:11 +0100

    Added js to highlight the selected answer button to the competition question

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,4 @@
-//
+//Update the value of tickets to add to basket
 function updateTicketCount(change){
 
   var sliderWithValue = document.getElementById("sliderWithValue");
@@ -28,7 +28,29 @@ function updateTicketCount(change){
     }
   }
  
+  //Update values
   ticketButtonCount.innerText = "Number of tickets: "+ ticketCount +"";
   sliderWithValue.value = ticketCount;
   ticketSliderCount.innerText = ticketCount;
+}
+
+
+////////////////////////// Set competition question button to active //////////////////////////////////////
+function answerQuestionsButtons(buttonNumber){
+
+  //Get every button in questionsButtons div
+  var answer1Button = document.getElementById("answer1Button");
+  var answer2Button = document.getElementById("answer2Button");
+  var answer3Button = document.getElementById("answer3Button");
+  var answer4Button = document.getElementById("answer4Button");
+
+  var buttons = new Array(answer1Button, answer2Button, answer3Button, answer4Button);
+
+  //Remove is active from every button
+  for (i = 0; i <= buttons.length - 1; i++) {
+    buttons[i].classList.remove("is-focused");
+  }
+  //set is-active to the button just clicked
+  buttons[buttonNumber-1].className += " is-focused";
+
 }
