7856636f9c1e944981a3e017a76895d7ca20c0bd
CH <connor-hughes@outlook.com>
2024-05-12 17:01:22 +0100

    Add res.locals.session

--- app.js
+++ app.js
@@ -61,6 +61,7 @@ app.use(doubleCsrfProtection);
 app.use((req, res, next) => {
     res.locals.csrfToken = generateToken(req, res);
     res.locals.login = req.isAuthenticated(); 
+    res.locals.session = req.session;
     next();
 });
 app.use(express.static(path.join(__dirname, 'public')));
