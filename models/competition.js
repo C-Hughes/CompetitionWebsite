01b221b15e6ca1b4be0c11d7e5ef77ca0a4c64d0
CH <connor-hughes@outlook.com>
2024-05-29 13:28:57 +0100

    Update competition schema to require entryCloseDate

--- models/competition.js
+++ models/competition.js
@@ -10,6 +10,7 @@ var competitionSchema = new Schema({
     price: {type: Number, required: true},
     discountPrice: {type: Number, required: false},
     drawDate: {type: Date, required: true},
+    entryCloseDate: {type: Date, required: true},
     currentEntries: {type: Number, required: true, default: 0},
     maxEntries: {type: Number, required: true},
     maxEntriesPerPerson: {type: Number, required: true, default: 100},
