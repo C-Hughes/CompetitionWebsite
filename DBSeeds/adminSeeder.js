e6304a32b1602b6bcbae6389df5983b90e02ab99
CH <connor-hughes@outlook.com>
2024-06-03 12:50:57 +0100

    Updateseeders to use env

--- DBSeeds/adminSeeder.js
+++ DBSeeds/adminSeeder.js
@@ -1,7 +1,7 @@
 var User = require('../models/user');
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/CompetitionMain', {
+mongoose.connect(process.env.MONGOOSE_CONNECT, {
     serverSelectionTimeoutMS: 5000
 });
 
--- DBSeeds/competitionSeeder.js
+++ DBSeeds/competitionSeeder.js
@@ -1,7 +1,7 @@
 var Competition = require('../models/competition');
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/CompetitionMain', {
+mongoose.connect(process.env.MONGOOSE_CONNECT, {
     serverSelectionTimeoutMS: 5000
 });
 
