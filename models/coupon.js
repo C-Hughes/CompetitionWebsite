a05c2818e33c99d0e9ce829efef9b430acf079d6
CH <connor-hughes@outlook.com>
2024-06-14 18:31:30 +0100

    Update coupon schema and coupons admin page

--- models/coupon.js
+++ models/coupon.js
@@ -4,9 +4,12 @@ var Schema = mongoose.Schema;
 var couponSchema = new Schema({
     userReference: {type: Schema.Types.ObjectId, ref: 'User', required: false},
     competitionReference: {type: Schema.Types.ObjectId, ref: 'Competition', required: false},
-    isSiteWide: {type: Boolean, required: true, default: false},
+    isSitewide: {type: Boolean, required: true, default: false},
     couponCode: {type: String, required: true},
+    couponAmount: {type: Number, required: false},
+    couponPercent: {type: Number, required: false},
     couponExpiryDate: {type: Boolean, required: true, default: false},
+    active: {type: Boolean, required: true, default: true},
     created: {type: Date, default: Date.now, required: true},
     lastUpdated: {type: Date, default: Date.now, required: true},
 });
