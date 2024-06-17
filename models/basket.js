b608658f37db2aacf8b36b4d532f271e73af0b94
CH <connor-hughes@outlook.com>
2024-06-17 22:15:45 +0100

    Update basket.js user check add .toString. Update variabled to apply % discount

--- models/basket.js
+++ models/basket.js
@@ -89,7 +89,8 @@ module.exports = function Basket(oldBasket){
         //console.log('Updating Basket...');
         this.basketTotalPrice = 0;
         this.basketSubtotalPrice = 0;
-        var totalReduction = 0;
+        var totalFlatReduction = 0;
+        var totalPercentReduction = 0;
         var messages = [];
         //Store competitionIds of each item in basket, if two competition IDs are the same, user is buying tickets for the same competition with different answers...
         //Extra checks required to make sure user can not buy more tickets than max allowed...  
@@ -237,9 +238,10 @@ module.exports = function Basket(oldBasket){
                         messages.push('Coupon Code Has Expired');
                     } else if (returnedCoupon.userReference){
                         //If it applies to a specific user check if current user is that user...
-                        if(user._id != returnedCoupon.userReference){
+
+                        if(user._id.toString() != returnedCoupon.userReference.toString()){
                             this.removeCoupon(coupon);
-                            messages.push('Invalid Coupon Code');
+                            messages.push('Invalid Coupon Code11');
                         }
                     } else if (returnedCoupon.competitionReference){
                         //If it applies to a specific competition, make sure that competition is in the basket...
@@ -266,40 +268,52 @@ module.exports = function Basket(oldBasket){
                             this.removeCoupon(coupon);
                             messages.push('This Coupon has Already Been Redeemed');
                         }
-                    }
+                    } else {
+                        //Coupon is valid
+                        
+                        //If coupon applies to specific competition
+                        if (returnedCoupon.competitionReference){
+                            for (var CID in this.items){
+                                //Get competition from basket item
+                                if (this.items[CID].item._id == returnedCoupon.competitionReference.id) {
+                                    //Reduce price of this item by the coupon amount
+                                    if(returnedCoupon.couponAmount){
+                                        this.items[CID].itemTotalPrice -= returnedCoupon.couponAmount;
+                                    } else if(returnedCoupon.couponPercent){
+                                        let discountDecimal = returnedCoupon.couponPercent / 100;
+                                        this.items[CID].itemTotalPrice *= (1 - discountDecimal);
+                                    }
 
-                    //If coupon applies to specific competition
-                    if (returnedCoupon.competitionReference){
-                        for (var CID in this.items){
-                            //Get competition from basket item
-                            if (this.items[CID].item._id == returnedCoupon.competitionReference.id) {
-                                //Reduce price of this item by the coupon amount
-                                if(returnedCoupon.couponAmount){
-                                    this.items[CID].itemTotalPrice -= returnedCoupon.couponAmount;
-                                } else if(returnedCoupon.couponPercent){
-                
-                                }
-
-                                //Check to make sure price isn't below 0
-                                if(this.items[CID].itemTotalPrice < 0){
-                                    this.items[CID].itemTotalPrice = 0;
+                                    //Check to make sure price isn't below 0
+                                    if(this.items[CID].itemTotalPrice < 0){
+                                        this.items[CID].itemTotalPrice = 0;
+                                    }
                                 }
                             }
-                        }
-                    } else {
-                        if(returnedCoupon.couponAmount){
-                            totalReduction = returnedCoupon.couponAmount
-                        } else if(returnedCoupon.couponPercent){
-
+                        } else {
+                            if(returnedCoupon.couponAmount){
+                                totalFlatReduction += returnedCoupon.couponAmount
+                            } else if(returnedCoupon.couponPercent){
+                                totalPercentReduction += returnedCoupon.couponPercent;
+                            }
                         }
                     }
+
+                    
                 }
                 //Update basket total price with coupons applied.
                 this.basketTotalPrice=0;
                 for (var id in this.items){
                     this.basketTotalPrice += this.items[id].itemTotalPrice;
                 }
-                this.basketTotalPrice-=totalReduction;
+                if(totalFlatReduction > 0){
+                    this.basketTotalPrice-=totalFlatReduction;
+                }
+                if(totalPercentReduction > 0){
+                    let discountDecimal = totalPercentReduction / 100;
+                    this.basketTotalPrice*= (1 - discountDecimal);
+                }
+                
             }
         } catch (err) {
             console.log(err);
